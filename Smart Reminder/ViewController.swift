ccbd9780bbbc8647466ae914e7fe15cb8ab68b9d
Aman Jain <amanj3777@gmail.com>
2020-07-07 19:48:31 +0530

    Initial Commit

---
+++ Smart Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smart Reminder
+//
+//  Created by Aman Jain on 07/07/20.
+//  Copyright © 2020 Aman Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
